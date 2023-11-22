adedff5983b925575548f65fe45ff25dcf767ac6
ochuks1 <cochuks@gmail.com>
2023-11-22 04:31:14 -0800

    For Tasks

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define BUFFER_SIZE 1024
+
+int main();
+
+#endif
