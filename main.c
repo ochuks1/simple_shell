c26ff1865134c2e0f6e679aa417c42afaf50fc21
ochuks1 <cochuks@gmail.com>
2023-12-06 23:05:33 -0800

    Task 0

---
+++ main.c
@@ -0,0 +1,7 @@
+#include "shell.h"
+
+int main(void)
+{
+    run_shell();
+    return (0);
+}
---
+++ shell.c
@@ -0,0 +1,7 @@
+#include "shell.h"
+#include <stdio.h>
+
+void run_shell(void)
+{
+    printf("Welcome to my shell!\n");
+}
