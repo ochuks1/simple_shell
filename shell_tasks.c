9e19b6f7388a81cd697530b8ec3e7701e94c296d
ochuks1 <cochuks@gmail.com>
2023-12-19 04:54:07 -0800

    Tasks

--- shell_tasks.c
+++ shell_tasks.c
@@ -12,52 +12,59 @@ extern char **environ;  /* External variable for environment variables */
  * execute_command - Execute the given command
  * @command: The command to execute
  */
-void execute_command(char *command) {
-    pid_t pid = fork();
-    if (pid == 0) {
-        char **args = malloc(2 * sizeof(char *));
-        if (args == NULL) {
-            perror("malloc failed");
-            exit(EXIT_FAILURE);
-        }
-
-        args[0] = command;
-        args[1] = NULL;
-
-        if (access(command, F_OK) != -1) {
-            execve(command, args, environ);
-        } else {
-            fprintf(stderr, "%s: command not found\n", command);
-            exit(EXIT_FAILURE);
-        }
-    } else if (pid < 0) {
-        perror("Fork failed");
-    } else {
-        wait(NULL);
-    }
+void execute_command(char *command)
+{
+pid_t pid = fork();
+if (pid == 0)
+{
+char **args = malloc(2 * sizeof(char *));
+if (args == NULL)
+{
+perror("malloc failed");
+exit(EXIT_FAILURE);
+}
+args[0] = command;
+args[1] = NULL;
+if (access(command, F_OK) != -1)
+{
+execve(command, args, environ);
+}
+else
+{
+fprintf(stderr, "%s: command not found\n", command);
+exit(EXIT_FAILURE);
+}
+}
+else if (pid < 0) 
+{	
+perror("Fork failed");
+}
+else
+{
+wait(NULL);
+}
 }
 
 /**
  * main - Simple shell program
  * Return: Always 0
  */
-int main(void) {
-    char *line = NULL;
-    size_t len = 0;
-
-    while (1) {
-        printf(":) ");
-        getline(&line, &len, stdin);
-        line[strcspn(line, "\n")] = 0;  /* Remove newline character */
-
-        if (strcmp(line, "exit") == 0) {
-            free(line);
-            exit(EXIT_SUCCESS);
-        }
-
-        execute_command(line);
-    }
-
-    free(line);
-    return 0;
+int main(void)
+{
+char *line = NULL;
+size_t len = 0;
+while (1)
+{
+printf(":) ");
+getline(&line, &len, stdin);
+line[strcspn(line, "\n")] = 0;  /* Remove newline character */
+if (strcmp(line, "exit") == 0)
+{
+free(line);
+exit(EXIT_SUCCESS);
+}
+execute_command(line);
+}
+free(line);
+return 0;
 }
