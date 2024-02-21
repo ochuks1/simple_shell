8faffb9f3267a5acb6a07b1ae31a14812d1d89d8
ochuks1 <cochuks@gmail.com>
2024-02-21 04:27:45 -0800

    Betty proud

---
+++ betty_proud.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    printf("Hello, Betty!\n");
+    return (0);
+}
